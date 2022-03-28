9767a8d521ed107e0986abe194551cc1f5390e06
rihabdhiab <rihabdhiab6@gmail.com>
2022-03-28 21:16:05 +0100

    my first react project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Form from './form';
+import 'bootstrap/dist/css/bootstrap.css'
+
+function App() {
+  return (
+    <div className="App">
+      
+      <h1>Registration Form </h1>
+      <Form />
+      
+    </div>
+  );
+}
+
+export default App;
